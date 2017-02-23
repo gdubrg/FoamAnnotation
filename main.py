ed27c76a0fa2ac01b76d0b48edaa3a98e479e929
berserkrambo <berserkrambo@gmail.com>
2017-02-23 17:26:34 +0100

    aggiornate istruzioni per backspace

--- main.py
+++ main.py
@@ -114,9 +114,11 @@ class FoamAnnotate:
 
 		cv2.putText(self.tmpImage, "Tasto SPACE: salva e prossimo frame", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1,
 					(225, 120, 0))
-		cv2.putText(self.tmpImage, "Tasto ESC: uscita (NON premere X per uscire)", (50, 100), cv2.FONT_HERSHEY_SIMPLEX,
+		cv2.putText(self.tmpImage, "Tasto ESC: uscita (NON premere X per uscire)", (50, 90), cv2.FONT_HERSHEY_SIMPLEX,
 					1, (0, 0, 200))
-		cv2.putText(self.tmpImage, "In caso di click errato, NON salvare ma ripetere i click dopo il 2", (50, 130),
+		cv2.putText(self.tmpImage, "In caso di click errato, NON salvare ma ripetere i click dopo il 2", (50, 110),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
+		cv2.putText(self.tmpImage, "Possibilita' di tornare indietro tramite backspace", (50, 130),
 					cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
 		cv2.putText(self.tmpImage, "Attenzione: cliccare prima linea sopra poi linea sotto", (50, 150),
 					cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 200))
